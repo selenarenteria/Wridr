75960a129cf21fb83ba449b7ab68664f61244bf6
Selena Renteria <selena.renteria18@gmail.com>
2022-03-09 16:07:12 -0600

    have my superuser created and also added one post through admin

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    image = models.CharField(max_length=250)
+    body_text = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['title']    
