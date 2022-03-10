2ff70fef6e8a52f1e4dbb14256b240780eb08e3c
Selena Renteria <selena.renteria18@gmail.com>
2022-03-10 10:58:37 -0600

    added a search bar for all post and configure it to search by title and it works

--- main_app/views.py
+++ main_app/views.py
@@ -3,21 +3,22 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView 
 from django.http import HttpResponse
+from .models import Post 
 
 # STATIC DATA
 # Class
-class Post:
-    def __init__(self, title, image, body_text):
-         self.title=title
-         self.image=image
-         self.body_text=body_text
+# class Post:
+#     def __init__(self, title, image, body_text):
+#          self.title=title
+#          self.image=image
+#          self.body_text=body_text
 
 
 # Seed data 
-posts = [
-    Post("SUGA day!", "https://i.imgur.com/eFmSPh9.jpeg", "Today is SUGA also known as Min Yoongi from BTS's birthday!!!"),
-    Post("SOPE appreciation post", "https://i.imgur.com/KQIPsCJs.jpeg", "SOPE is the best duo in BTS!")
-]
+# posts = [
+#     Post("SUGA day!", "https://i.imgur.com/eFmSPh9.jpeg", "Today is SUGA also known as Min Yoongi from BTS's birthday!!!"),
+#     Post("SOPE appreciation post", "https://i.imgur.com/KQIPsCJs.jpeg", "SOPE is the best duo in BTS!")
+# ]
 # Create your views here.
 
 class Home(TemplateView):
@@ -38,6 +39,12 @@ class AllPost(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['posts'] = posts
+        # context['posts'] = posts
+        title = self.request.GET.get("title")
+        print(title)
+        if title != None:
+            context['posts'] = Post.objects.filter(title__icontains=title)
+        else:    
+            context['posts'] = Post.objects.all()
         return context
 
