3eb4c9a999645b0c1188ceaa4cf5cb618f56d763
Selena Renteria <selena.renteria18@gmail.com>
2022-03-13 20:22:50 -0500

    added user authentication able to login with superuser on frontend and redirects to all post page

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 
 
@@ -8,7 +8,7 @@ class Post(models.Model):
     image = models.CharField(max_length=250)
     body_text = models.TextField(max_length=500)
     created_at = models.DateTimeField(auto_now_add=True)
-    
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
