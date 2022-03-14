db4b48f946b7fca61072122f59d5b70191987001
Selena Renteria <selena.renteria18@gmail.com>
2022-03-14 15:34:37 -0500

    Adds heroku deployment config

--- wridr_project/settings.py
+++ wridr_project/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv 
 load_dotenv()
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +130,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "/posts/"
 LOGOUT_REDIRECT_URL = "/"
+
+django_heroku.settings(locals())
